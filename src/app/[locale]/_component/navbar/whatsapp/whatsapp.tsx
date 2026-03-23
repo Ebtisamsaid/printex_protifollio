d802041c9ff3ec37adf75ddde18cdca928ba5668
Ebtisam.elnahas20@gmail.com <Ebtisam.elnahas20@gmail.com>
2026-03-23 20:27:21 +0200

    edit social

--- src/app/[locale]/_component/navbar/whatsapp/whatsapp.tsx
+++ src/app/[locale]/_component/navbar/whatsapp/whatsapp.tsx
@@ -8,12 +8,12 @@ export default function WhatsAppButton() {
 const whatsappUrl = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`;
   return (
     <>
-  <div className="flex justify-between gap-2 fixed bottom-6 end-6 z-[999]">
+  <div className="flex-col justify-between gap-2  fixed bottom-6 end-6 z-[999]">
       <Link
       href={whatsappUrl}
        target="_blank"
       rel="noopener noreferrer"
-      className=" flex items-center justify-center w-14 h-14 bg-[#25D366] text-white rounded-full shadow-lg hover:scale-110 hover:bg-[#128C7E] transition-all duration-300 group"
+      className=" flex items-center justify-center  w-14 h-14 bg-[#25D366] text-white rounded-full shadow-lg hover:scale-110 hover:bg-[#128C7E] transition-all duration-300 group"
       aria-label="Contact us on WhatsApp"
     >
       <FaWhatsapp className="w-8 h-8" />
@@ -26,7 +26,7 @@ const whatsappUrl = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(mess
       href="https://www.facebook.com/Printexegy"
        target="_blank"
       rel="noopener noreferrer"
-      className="flex items-center justify-center w-14 h-14 bg-blue-900 text-white rounded-full shadow-lg hover:scale-110 hover:bg-blue-700 transition-all duration-300 group"
+      className="flex items-center justify-center my-2 w-14 h-14 bg-blue-900 text-white rounded-full shadow-lg hover:scale-110 hover:bg-blue-700 transition-all duration-300 group"
       aria-label="Contact us on facebook"
     >
       <FaFacebook className="w-8 h-8" />
