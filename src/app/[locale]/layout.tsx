76b4dc1cc460eb92473efe756142ee241256d7f3
Ebtisam.elnahas20@gmail.com <Ebtisam.elnahas20@gmail.com>
2026-04-11 21:19:06 +0200

    add fb pixel

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -6,6 +6,7 @@ import { getMessages } from "next-intl/server";
 import { NextIntlClientProvider } from "next-intl";
 import Navbar from "./_component/navbar/navbar";
 import Contact from "./_component/footer/footer";
+import Script from "next/script";
 
 
 export default async function RootLayout({
@@ -26,6 +27,39 @@ export default async function RootLayout({
   if (!locales.includes(locale)) notFound();
   return (
     <html lang={lang}  dir={direction}>
+      <head>
+<Script id="fb-pixel" strategy="afterInteractive">
+{/* !function(f,b,e,v,n,t,s)
+{
+if(f.fbq)return;
+n=f.fbq=
+function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '1464903172093262');
+fbq('track', 'PageView'); */}
+
+{`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '1464903172093262');
+            fbq('track', 'PageView');
+          `}
+</Script>
+<noscript><img height="1" width="1" style={{ display: 'none' }}
+src="https://www.facebook.com/tr?id=1464903172093262&ev=PageView&noscript=1"
+/></noscript>
+      </head>
       <body className="overflow-x-hidden">
         <NextIntlClientProvider messages={messages} locale={locale}>
         <Navbar/>
